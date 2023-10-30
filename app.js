ad58bd7b35f9b99c213e2a6553d895a1d3b17669
harshugpt <uharshgpt2912@gmail.com>
2023-10-30 16:53:27 +0530

    event & util module with eventEmitter

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-/*var stuff = require("./stuff");
-
-console.log(stuff.counter(5));
-console.log(stuff.adder(2, 4));
-*/
-
-//event emitter
+//adding custom event emitter to the object constructor
 
 var events = require("events");
+var util = require("util");
 
-var myEmitter = new events.EventEmitter();
+var Person = function (name) {
+  this.name = name;
+};
 
-myEmitter.on("someEvent", function (mssg) {
-  console.log(mssg);
+util.inherits(Person, events.EventEmitter);
+
+var harsh = new Person("harsh");
+var harshu = new Person("harshu");
+var harshi = new Person("harshi");
+
+var people = [harsh, harshu, harshi];
+
+people.forEach(function (person) {
+  person.on("speak", function (mssg) {
+    console.log(person.name + "says:" + mssg);
+  });
 });
 
-myEmitter.emit("someEvent", "the event was emitted");
+harsh.emit("speak", "hello");
