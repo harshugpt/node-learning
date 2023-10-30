f908082a6207d76d5b4b82e8362288121b6c1cef
harshugpt <uharshgpt2912@gmail.com>
2023-10-30 16:50:40 +0530

    node module export&require

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ console.log(stuff.adder(2, 4));
 */
 
 //event emitter
-/*
+
 var events = require("events");
 
 var myEmitter = new events.EventEmitter();
@@ -15,28 +15,3 @@ myEmitter.on("someEvent", function (mssg) {
 });
 
 myEmitter.emit("someEvent", "the event was emitted");
-*/
-//adding custom event emitter to the object constructor
-
-var events = require("events");
-var util = require("util");
-
-var Person = function (name) {
-  this.name = name;
-};
-
-util.inherits(Person, events.EventEmitter);
-
-var harsh = new Person("harsh");
-var harshu = new Person("harshu");
-var harshi = new Person("harshi");
-
-var people = [harsh, harshu, harshi];
-
-people.forEach(function (person) {
-  person.on("speak", function (mssg) {
-    console.log(person.name + "says:" + mssg);
-  });
-});
-
-harsh.emit("speak", "hello");
